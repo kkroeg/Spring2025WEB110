850d857f5ac86307132192a9bf6ac9a23142b444
kkroeg <157152523+kkroeg@users.noreply.github.com>
2025-03-10 12:28:19 -0500

    midterm js

---
+++ midtermproject/main.js
@@ -0,0 +1,27 @@
+document.getElementById('purple').onclick = partyPurple
+document.getElementById('blue').onclick = partyBlue
+document.getElementById('green').onclick = partyGreen
+document.getElementById('pink').onclick = partyPink
+
+
+function partyPurple() {
+  document.querySelector('body').style.backgroundColor = 'purple'
+  document.querySelector('body').style.color = 'white'
+}
+
+function partyBlue() {
+  document.querySelector('body').style.backgroundColor = 'blue'
+  document.querySelector('body').style.color = 'white'
+}
+
+function partyGreen() {
+  document.querySelector('body').style.backgroundColor = 'green'
+  document.querySelector('body').style.color = 'white'
+}
+
+function partyPink() {
+  document.querySelector('body').style.backgroundColor = 'pink'
+  document.querySelector('body').style.color = 'white'
+}
+
+
